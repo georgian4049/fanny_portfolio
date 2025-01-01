b25d1647cf1e134c262a775e6c279e26372a755f
ayush <cadet4049@gmail.com>
2025-01-01 22:34:10 +0100

    minor fix

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import PortfolioBlock from "./PortfolioBlock";
 import {Box, Grid} from "@mui/material";
-import {info} from "../../info/Info";
 
 export default function Portfolio({innerRef}) {
     return (
